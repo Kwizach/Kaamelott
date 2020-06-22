b563618e737d6c8d0de21914130c35a2eaf620f1
Clement <clement.rabourdin@myeggo.com>
2020-06-22 02:03:13 +0200

    Initial Commit

---
+++ Kaamelott/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Kaamelott
+//
+//  Created by Clement Rabourdin on 22/06/2020.
+//  Copyright © 2020 Clement Rabourdin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
